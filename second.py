e49e3ace79f7ad1146772a4fb3e48598d005f750
Clare Groninger <claregro.61461@redlandsschools.net>
2018-09-10 15:25:39 +0000

    Wokring on Second

---
+++ second.py
@@ -0,0 +1,12 @@
+"""
+Triple quotes allow for multiple lines of comments
+"""
+
+
+age = input("What is your age?")
+
+if (age>18):
+    print ("Then go Vote!")
+
+if (age<18):
+    print ("Just a few more years until you can vote")
