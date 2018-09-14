fd885b507328576ce6889d2c1423f65675fd0720
Clare Groninger <claregro.61461@redlandsschools.net>
2018-09-14 15:03:29 +0000

    Pythagorean startup

---
+++ Groninger_pythagorean.py
@@ -0,0 +1,16 @@
+
+
+"""
+Write a program that will ask the user to enter their name and respond with a greeting using this name
+
+"""
+
+name=input("What is your name? ")
+print ("Nice to meet you, " + name)
+
+a=float(input("Please give me a length for a triangle: "))
+
+b=float(input("Please give me another length: "))
+
+
+print ("That means the hypotenuse is " + str((a**2 + b**2)**.5))
--- evenOrOdd.py
+++ evenOrOdd.py
@@ -2,8 +2,18 @@
 
 num= int(input("Please Enter a Number "))
 
-if (num%2==0):
+if (num%2 == 0):
     print ("That's even!")
 
-if (num%2==1):
+if (num%2 == 1):
     print ("That's odd!")
+    
+    
+num= float(input("Please Enter a Number (Integer please):"))
+
+if (num%2==0):
+    print ("That's even!")
+    
+elif (num%2==1):
+    print ("That's odd!")
+    
